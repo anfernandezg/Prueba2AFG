b521fe68925fe621a09d9521b342e9150b2dc5c7
Andres Fernandez <an.fernandezg@duocuc.cl>
2024-11-06 12:58:17 -0300

    Comenzando el proyecto de prueba 2

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'prueba2AFG',
+  webDir: 'www'
+};
+
+export default config;
